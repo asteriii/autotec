11c456bb20f1eddc6e4306552433a697bd1f358a
aanjdokey <aquino.aanj@gmail.com>
2025-10-25 00:29:28 +0800

    update adminDash to fix db connection

--- AdminSide/adminDash.php
+++ AdminSide/adminDash.php
@@ -1,15 +1,7 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "autotec";
+ 
+require_once '../db.php';
 
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
 
 // Get current date for filtering
 $today = date('Y-m-d');
