eeb367842e2f59f66aca3c5b1d6444b1dad27aba
aanjdokey <aquino.aanj@gmail.com>
2025-10-28 19:05:03 +0800

    updated logout.php to link with the new index url, and fixed issues with modals

--- process/logout.php
+++ process/logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: http://localhost/autotec/index.php');
+header('Location: https://autotec-production.up.railway.app/index.php');
 exit();
 ?>
